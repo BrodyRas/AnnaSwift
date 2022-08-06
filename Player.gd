eaf09c1e6c4e9e8293fd1dbb5b00bb3b6e5affcb
Brody Rasmussen <brodyras@fb.com>
2022-08-06 09:55:05 -0600

    Added warp movement

--- Player.gd
+++ Player.gd
@@ -1,10 +1,5 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 export var turnSpeed = 1.0
 var turningLeft = false
 var turningRight = false
@@ -12,14 +7,20 @@ var turningRight = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Get the list of WalkNodes in the scene, and connect their signals to our move function
+	var children = get_tree().get_root().find_node("WalkNodes", true, false).get_children()
+	for child in children:
+		child.connect("walkAreaClicked", self, "_on_WalkArea_Clicked")
 
+# When a WalkArea is clicked, warp to its global position
+func _on_WalkArea_Clicked(position):
+	self.global_transform.origin = position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _physics_process(delta):
+	handleTurning(delta)
+
+func handleTurning(delta):
 	if turningLeft:
 		self.rotate_y(deg2rad(turnSpeed))
 		
@@ -28,12 +29,9 @@ func _physics_process(delta):
 
 func _on_Left_Area_mouse_entered():
 	turningLeft = true
-
 func _on_Left_Area_mouse_exited():
 	turningLeft = false
-
 func _on_Right_Area_mouse_entered():
 	turningRight = true
-
 func _on_Right_Area_mouse_exited():
 	turningRight = false
--- WalkNode.gd
+++ WalkNode.gd
@@ -2,24 +2,10 @@ extends Spatial
 
 signal walkAreaClicked
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+# Once the area is clicked, emit a signal with global position, so the player can warp to this position
 func _on_MoveArea_input_event(camera, event, position, normal, shape_idx):
 	event = event as InputEventMouseButton
 	if event != null:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
-			print('clicked')
-			emit_signal("walkAreaClicked")
+			print('clicked', self.global_transform.origin)
+			emit_signal("walkAreaClicked", self.global_transform.origin)
